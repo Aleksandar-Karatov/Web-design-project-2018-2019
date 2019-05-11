29b49a876669439d6af7be0bb72aabcfa9c4d885
Alex-K-2020v <32852359+Alex-K-2020v@users.noreply.github.com>
2019-05-11 20:25:54 +0300

    Easter egg added

---
+++ main_site_script.js
@@ -0,0 +1,20 @@
+var startTime, endTime;
+function hoverIn()
+{
+
+	startTime = Date.now();
+}
+function hoverOut()
+{	endTime = Date.now();
+
+	var powered=document.getElementById("bottom");
+	var timeDiff = endTime - startTime;
+	if(endTime-startTime>=1000 && powered.style.color!="white" )
+	{
+		powered.style.color= "black";
+	}
+	else
+	{
+		powered.style.color="white";
+	}
+}
